0fb917239f76b93b925224f1d832f61df9afb85e
Yasmin Vitoria da Silva Sobrinho <ysobrinho79@gmail.com>
2026-06-17 11:05:04 -0300

    exemplo com else if

---
+++ l041q06.js
@@ -0,0 +1,8 @@
+let num1 = 30;
+let num2 = 12;
+
+if (num1 > num2){
+ console.log("a diferença é: "+ (num1 - num2));   
+} else {
+    console.log("a diferença é: "+ (num2 - num1));
+} 
---
+++ l041q07.js
@@ -0,0 +1,13 @@
+//Desenvolver um programa que pergunte um valor inteiro positivo ou negativo, e exiba como resposta o módulo
+//deste valor, ou seja, o número lido como sendo po
+
+let num ;
+console.log("informe um número: ")
+num = 10;
+console.log(`Número informado ${num}`);
+
+if (num > 0) {
+    console.log(` ${num} é positivo`);
+} else if (num > 0) {
+   console.log(` ${num} é negativo`); 
+}
