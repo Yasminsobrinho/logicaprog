c2512b2e48d87bd5d0431599977bb315877dda41
Yasmin Vitoria da Silva Sobrinho <ysobrinho79@gmail.com>
2026-06-17 11:12:42 -0300

    alteracoes no l041q07.js e no l041q09.js

--- l041q07.js
+++ l041q07.js
@@ -1,5 +1,5 @@
 //Desenvolver um programa que pergunte um valor inteiro positivo ou negativo, e exiba como resposta o módulo
-//deste valor, ou seja, o número lido como sendo po
+//deste valor, ou seja, o número lido como sendo positivo
 
 let num ;
 console.log("informe um número: ")
@@ -8,6 +8,6 @@ console.log(`Número informado ${num}`);
 
 if (num > 0) {
     console.log(` ${num} é positivo`);
-} else if (num > 0) {
+} else if (num < 0) {
    console.log(` ${num} é negativo`); 
 }
