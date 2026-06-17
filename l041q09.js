809dd7fb1ff23d4041e31d16116524169dd05058
Yasmin Vitoria da Silva Sobrinho <ysobrinho79@gmail.com>
2026-06-17 11:13:36 -0300

    alteracoes no l041q09.js

--- l041q09.js
+++ l041q09.js
@@ -8,7 +8,7 @@ console.log(`Número informado ${num}`);
 
 if (num > 0) {
     console.log(` ${num} é positivo`);
-} else if (num > 0) {
+} else if (num < 0) {
    console.log(` ${num} é negativo`); 
 } else {
     console.log(` ${num} é nulo`);
