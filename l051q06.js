d4bc771fb370a689fdea1aaeb06d37916d755760
Yasmin Vitoria da Silva Sobrinho <ysobrinho79@gmail.com>
2026-06-18 11:09:04 -0300

    while

--- l051q06.js
+++ l051q06.js
@@ -1,3 +1,12 @@
 /*Desenvolver um programa que leia um número n qualquer menor ou igual a 50 e apresente o valor obtido da
 multiplicação sucessiva de n por 3 enquanto o produto for menor que 250. (n x 3; n x 3 x 3; n x 3 x 3 x 3 etc...).*/
 
+let n = 5;
+
+while ( n <= 250 ){
+    n = n * 3
+    if (n < 250 ){
+         console.log (n);
+    }
+
+}
